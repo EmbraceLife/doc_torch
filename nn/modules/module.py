856e0ee45f1819506643aab89a1734414f69ff67
EmbraceLife <1227561934@qq.com>
2019-05-26 12:51:22 +0800

    Sequential.__call__

--- nn/modules/module.py
+++ nn/modules/module.py
@@ -526,6 +526,9 @@ class Module(object):
         ----what
         `Sequential.__call__(*input, **kwargs)`:
             0. when an instance of Sequential is called like a func
+            1. basically run avaliable hook functions and forward function
+
+        ----procedures    
             1. inherit from `Module.__init__()`
             2. if there is `self._forward_pre_hooks` available, run the hook
             3. if `torch._C._get_tracing_state()` is true, do `self._slow_forward`
